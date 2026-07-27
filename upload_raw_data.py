f76007c7f1aff583c6bf41322e86a0515c44035f
Sakeenah Aderinto <sakeenahjustcodes@gmail.com>
2026-07-27 21:57:35 +0100

    feat(pipeline): upload data from source to bronze container in storage account

---
+++ upload_raw_data.py
@@ -0,0 +1,46 @@
+import os
+from azure.storage.blob import BlobServiceClient
+from dotenv import load_dotenv
+from pathlib import Path
+
+location = Path(__file__).resolve().parent
+
+project_root = location
+
+load_dotenv()
+
+ACCOUNT_KEY = os.getenv('ACCOUNT_KEY')
+ACCOUNT_NAME = 'urbancitystorage'
+CONTAINER_NAME = 'bronze'
+
+LOCAL_FILE_PATH = project_root / "data" / "311_urban_service_requests.csv"
+
+BLOB_NAME = "311_urban_service_requests.csv"
+
+
+def upload_data():
+    try:
+        blob_service_client = BlobServiceClient(
+            account_url=f"https://{ACCOUNT_NAME}.blob.core.windows.net",
+            credential=ACCOUNT_KEY
+        )
+
+        blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=BLOB_NAME)
+
+        print(f"starting file upload from {LOCAL_FILE_PATH}")
+
+        with open(LOCAL_FILE_PATH, 'rb') as data:
+            blob_client.upload_blob(
+                data,
+                overwrite=True,
+                max_concurrency=4
+            )
+
+            print(f"upload successful. File loaded to {CONTAINER_NAME}")
+
+    except Exception as e:
+        print(e)
+
+    return None
+
+upload_data()
