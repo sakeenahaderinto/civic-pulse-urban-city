31fd7b9723af26da134934663fe2730921de17d9
Sakeenah Aderinto <sakeenahjustcodes@gmail.com>
2026-07-28 09:55:23 +0100

    feat(pipeline): transform raw data from bronze container and load it to the silver container

---
+++ transform.py
@@ -0,0 +1,44 @@
+import polars as pl
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+storage_options = {
+    'account_name': 'urbancitystorage',
+    'account_key': os.getenv('ACCOUNT_KEY')
+}
+
+def transform():
+    source_uri = 'az://bronze/311_urban_service_requests.csv'
+    target_uri = 'az://silver/311_urban_service_requests.parquet'
+
+    df = pl.scan_csv(source_uri, storage_options=storage_options)
+
+    column_mapping = {
+        "Created Date": "created_date",
+        "Closed Date": "closed_date",
+        "Problem (formerly Complaint Type)": "problem_detail",
+        "Location Type": "location_type",
+        "Incident Address": "incident_address",
+        "City": "city",
+        "Borough": "borough",
+        "Latitude": "latitude",
+        "Longitude": "longitude"
+    }
+
+    df_refined = df.select(
+        [pl.col(old).alias(new) for old, new in column_mapping.items()]
+    )
+
+    df_refined.sink_parquet(
+        target_uri,
+        storage_options=storage_options,
+        compression="snappy"
+    )
+
+    print(f"data loaded to {target_uri}")
+
+    return None
+
+transform()
